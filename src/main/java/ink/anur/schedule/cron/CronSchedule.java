378a36a59080db9b4994b4f916a2c6410a98bd40
Anur <anurnomeru@163.com>
2020-03-02 16:39:31 +0800

    引入cron相关代码

--- src/main/java/ink/anur/schedule/cron/CronSchedule.java
+++ src/main/java/ink/anur/schedule/cron/CronSchedule.java
@@ -20,9 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- *****************************************************************************
- * The MIT License applies to this file only.
- *****************************************************************************
  */
 package ink.anur.schedule.cron;
 
